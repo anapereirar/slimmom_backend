12ecbb50afed3a86579279a4ae2135afab69afb4
anapereirar <ana.pereirar@hotmail.com>
2023-11-03 22:23:14 -0500

    Trabajo de backend

---
+++ routes/api/dailyNorma.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const { auth, ctrlWrapper, validation } = require("../../middlewares");
+const { joiDailyNormaSchema: schema } = require("../../models/user");
+const {
+  privateR,
+  publicR,
+} = require("../../controllers/users/getDailyNorma.js");
+
+router.post("/private", auth, validation(schema), ctrlWrapper(privateR));
+
+router.post("/public", validation(schema), ctrlWrapper(publicR));
+
+module.exports = router;
